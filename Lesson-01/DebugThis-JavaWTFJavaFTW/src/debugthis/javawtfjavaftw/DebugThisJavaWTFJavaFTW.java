b2381e93d9a6ece257a4072e02dba2230c4de8c4
Michael Fudge <mafudge@syr.edu>
2013-07-26 13:20:04 -0400

    added the rest of lesson 01

---
+++ Lesson-01/DebugThis-JavaWTFJavaFTW/src/debugthis/javawtfjavaftw/DebugThisJavaWTFJavaFTW.java
@@ -0,0 +1,29 @@
+/*
+ * DEBUG THIS! Exercise
+ * 
+ * Learning to read & debug existing code is an important skill.
+ * Fix the errors in this program. 
+ * Try not to re-write the program, just correct what's wrong.
+ * When you get it working, it should display exactly this:
+ * 
+ * 
+ * PROGRAM OUTPUT
+ * 
+ *  What the F-*bleep*, Java?
+ *  Oops, my bad.
+ *  Java, for the win!
+ */
+package debugthis.javawtfjavaftw;
+
+public class DebugThisJavaWTFJavaFTW {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println "What the F-*bleep*, Java?";
+        System.out.print("Oops,")
+        System.out.printf("my bad.");
+        System.println("Java, for the win!");
+    }
+}
---
+++ Lesson-01/PrintingPower/src/printingpower/PrintingPower.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package printingpower;
+
+/**
+ *
+ * @author mafudge
+ */
+public class PrintingPower {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Let's print a huge smiley face!
+        System.out.println("XXXXXXXXXXX");
+        System.out.println("X  _   _  X");
+        System.out.println("X  0   0  X");
+        System.out.println("X    ^    X");
+        System.out.println("X  \\___/  X"); // "\" is the escape character
+        System.out.println("X         X");
+        System.out.println("XXXXXXXXXXX");
+        
+    }
+}
