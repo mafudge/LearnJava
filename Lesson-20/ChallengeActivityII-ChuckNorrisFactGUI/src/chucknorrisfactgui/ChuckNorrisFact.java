740095481d19d413e6941fba9d12ab13a67d9fae
Michael Fudge <mafudge@syr.edu>
2013-10-02 17:14:02 -0400

    Finishing up lessons

--- Lesson-20/ChallengeActivityII-ChuckNorrisFactGUI/src/chucknorrisfactgui/ChuckNorrisFact.java
+++ Lesson-20/ChallengeActivityII-ChuckNorrisFactGUI/src/chucknorrisfactgui/ChuckNorrisFact.java
@@ -1,4 +1,4 @@
-package chucknorrisfact;
+package chucknorrisfactgui;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
---
+++ Lesson-20/ChuckNorrisFact/src/chucknorrisfact/ChuckNorrisFactRun.java
@@ -0,0 +1,11 @@
+package chucknorrisfact;
+
+public class ChuckNorrisFactRun {
+
+    public static void main(String[] args) {
+        ChuckNorrisFact cnf = new ChuckNorrisFact();
+        System.out.println(cnf.getRandomJoke());
+    }
+
+}
+
